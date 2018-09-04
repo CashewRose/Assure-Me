8344a4960582153826305e81b4cd8f31af139b10
CashewRose <danieagnoletti@gmail.com>
2018-09-04 14:11:11 -0500

    Added docstring and made pull request issue template for future ease of PR use

--- djangocap/Assure_Me/views/initial_view.py
+++ djangocap/Assure_Me/views/initial_view.py
@@ -2,5 +2,14 @@ from django.shortcuts import render
 from django.template import RequestContext
 
 def initial(request):
+    """This function gets the view to the initial.html template and returns it to the requester
+    
+    Arguments:
+        None
+    
+    Author:
+        Cashew Rose
+    """
+
     template_name = 'initial.html'
     return render(request, template_name, {})
