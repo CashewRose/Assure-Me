2cb4ff62182ea3c609f4d3415fed98d385588b75
CashewRose <danieagnoletti@gmail.com>
2018-09-06 14:22:49 -0500

    Account edit form updates, saves, and redirects correctly.

--- djangocap/Assure_Me/views/account_edit_view.py
+++ djangocap/Assure_Me/views/account_edit_view.py
@@ -1,25 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from Assure_Me.forms import AccountEditForm
 
  
 def account_edit_view(request):
+
+    """This will give the users a form where they can update their account information. On submit it will update their account information and save it to the database. 
+    
+    Method Arguments:
+        request -- The full HTTP request object
+
+    Author: 
+        Cashew Rose
+    """
     
     ##Runs only when form is submitted
     if request.method == "POST":
-        user_form = EditFormUser(request.POST, instance=request.user)
-        customer_form = EditFormCustomer(request.POST, instance=customer_instance)
+        user_form = AccountEditForm(request.POST, instance=request.user)
 
         ##Checks everything in both forms is filled out correctly and saves them with the new information. Sends them back to the main account page when finished.
-        if user_form.is_valid() and customer_form.is_valid():
+        if user_form.is_valid():
             user = user_form.save()
-            customer = customer_form.save()
 
-            return redirect('website:account_home')
+            return redirect('Assure_Me:account')
 
     else: 
-        formCustomer= EditFormCustomer(instance=customer_instance)
-        formUser = EditFormUser(instance=request.user)
+        formUser = AccountEditForm(instance=request.user)
 
     ##Displays the page with the forms    
-    return render(request, 'account/account_edit.html', {'customer_form': formCustomer,
-    'user_form': formUser})
+    return render(request, 'account_edit.html', {'user_form': formUser})
