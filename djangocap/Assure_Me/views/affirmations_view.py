65edc792a1da90e0dc46b7ad01a674c72dc8d465
CashewRose <danieagnoletti@gmail.com>
2018-09-07 09:57:37 -0500

    Added docstring and comments to code in affirmations_view.py

--- djangocap/Assure_Me/views/affirmations_view.py
+++ djangocap/Assure_Me/views/affirmations_view.py
@@ -5,11 +5,21 @@ from Assure_Me.models import Affirmation
 
 @login_required
 def affirmations_view(request):
+    """This function gets the view to the affirmations.html template and returns it to the requester.
+        It filters and grabs all of the sepcific users affirmations and sends that to the template accordingly.
+    
+    Method Arguments:
+        request -- The full HTTP request object
+        
+    Author:
+        Cashew Rose
+    """
 
     try:
         affirmations = Affirmation.objects.filter(user=request.user.id)
         print(affirmations)
         return render(request, "affirmations.html", {'affirmations': affirmations})
-        
+
+    # Accounts for if there are no current affirmations for that user
     except ObjectDoesNotExist:
         return render(request, "affirmations.html")
