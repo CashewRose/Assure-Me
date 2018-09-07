5cec78be7ddcd0a10d44d7df5af3b0362e3ed85d
CashewRose <danieagnoletti@gmail.com>
2018-09-07 09:37:46 -0500

    Affirmation view accounts for all the users affirmations and also if it is empty. No 404 errors provided.

--- djangocap/Assure_Me/views/affirmations_view.py
+++ djangocap/Assure_Me/views/affirmations_view.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
 from Assure_Me.models import Affirmation
 
 @login_required
 def affirmations_view(request):
 
-    affirmations = get_list_or_404(Affirmation, user=request.user.id)
-    print(affirmations)
-    return render(request, "affirmations.html", {'affirmations': affirmations})
+    try:
+        affirmations = Affirmation.objects.filter(user=request.user.id)
+        print(affirmations)
+        return render(request, "affirmations.html", {'affirmations': affirmations})
+    except ObjectDoesNotExist:
+        return render(request, "affirmations.html")
