7a5b5141038751e43792422df48dbd559b90ac00
CashewRose <danieagnoletti@gmail.com>
2018-09-10 15:53:31 -0500

    Twilio resonds and sends messages on terminal command.

--- djangocap/Assure_Me/urls.py
+++ djangocap/Assure_Me/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
     url(r'^affirmation_new$', views.new_affirmation_view, name='new_affirmation'),
     url(r'^affirmation_delete/(?P<Affirmation_Id>\d+)$', views.delete_affirmation_view, name='delete_affirmation'),
     url(r'^affirmation_edit/(?P<Affirmation_Id>\d+)$', views.edit_affirmation_view, name='edit_affirmation'),
+    url(r'^phone$', views.sms_response, name='text'),
 ]
--- djangocap/djangocap/settings.py
+++ djangocap/djangocap/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9(05$&wmzmm8q%n2l4qat($*x29y0#kitj%4b(%mp*5*&9k*h_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.ngrok.io']
 
 # Application definition
 
