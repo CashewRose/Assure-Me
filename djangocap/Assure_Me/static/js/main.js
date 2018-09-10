87402bf01beaeee8c2409810e8908e69898f4368
CashewRose <danieagnoletti@gmail.com>
2018-09-10 12:01:11 -0500

    Hides the form element and shows it when clicking the edit button.

--- djangocap/Assure_Me/static/js/main.js
+++ djangocap/Assure_Me/static/js/main.js
@@ -1,1 +1,18 @@
 console.log("Boop")
+
+forms = document.getElementsByClassName("affirmation_submit")
+edit_button = document.getElementsByClassName("edit_button")
+
+Array.from(forms).forEach(function(form) {
+    form.parentNode.style.display = 'none';
+})
+
+Array.from(edit_button).forEach(function(edit) {
+    edit.parentElement.style.display = '';
+    edit.addEventListener("click", function(){
+        parent = edit.parentElement.parentElement;
+        edit.parentElement.style.display = 'none';
+        form = parent.children[1]
+        form.style.display = '';
+    })
+})
