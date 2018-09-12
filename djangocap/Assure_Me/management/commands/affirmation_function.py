e4879bf95e0b60cb6fbd0a3851014a53214912a0
CashewRose <danieagnoletti@gmail.com>
2018-09-12 15:57:41 -0500

    Python script function now sends correctly.

--- djangocap/Assure_Me/management/commands/affirmation_function.py
+++ djangocap/Assure_Me/management/commands/affirmation_function.py
@@ -1,13 +1,10 @@
 import os
+import random
 
 from django.core.management.base import BaseCommand
-from django.core.exceptions import ObjectDoesNotExist
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
 
 from Assure_Me.models import User, Affirmation
 
-from twilio.twiml.messaging_response import MessagingResponse
 from twilio.rest import Client
 
 # Account Sid and Auth Token from twilio.com/console
@@ -18,22 +15,22 @@ client = Client(account_sid, auth_token)
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        try:
-            affirm_users_to_send = User.objects.filter(confirmed=True)
-            print(users_affirmations)
+
+        affirm_users_to_send = User.objects.filter(confirmed=True)
+        if len(affirm_users_to_send) !=0:
             for user in affirm_users_to_send:
                 number = '+1' + user.phone_number
                 affirm = Affirmation.objects.filter(user_id=user.id)
 
+            if len(affirm) != 0:
                 message = client.messages \
                     .create(
-                            body=affirm[0].affirmation,
+                            body=random.choice(affirm).affirmation,
                             from_='+16158662463',
                             to=number
                         )
-
-        except ObjectDoesNotExist:
-            print("No one to send messages to today!")
-
-
-
+            else:
+                print("This user hasn't put in any affirmations yet!")
+        else:
+            print("No users to send messages to today!")
+                        
